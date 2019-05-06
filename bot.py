d7e092126d6080744eed9cb96cfcb5eab34ac606
Tristan WAGNER <t.wagner@adveez.com>
2019-05-06 21:10:06 +0200

    added linux support and updated to latest discordpy

--- bot.py
+++ bot.py
@@ -3,10 +3,8 @@ import logging, os, discord, asyncio,sys,csv,traceback
 from selenium import webdriver
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
-FIREFOX_PATH = r'C:\Program Files\Mozilla Firefox\firefox.exe'
-GECKODRIVER_PATH = r'C:\geckodriver.exe'
-cachefolder = os.getcwd() + '\cache\\'
-cachetrigger = True;
+cachefolder = os.getcwd() + '/cache/'
+cachetrigger = True
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
@@ -29,12 +27,12 @@ async def on_ready():
 @client.event
 async def on_message(message):
 	if message.content.startswith('!help'):
-		await client.send_message(message.channel, 'Find item\'s tooltip :\n- "!finditem <NAME/ID>" or "!fi <NAME/ID>" - Example -> !finditem thunderfury\n- "!finditem #VANILLAGAMINGITEMID" - Example -> !finditem 18402')
-		await client.send_message(message.channel, 'Finding Player :\n- "!findplayer <NAME>" or "!fp <NAME>"')
-		await client.send_message(message.channel, 'Finding Quest :\n- "!findquest <NAME/ID>" or "!fq <NAME/ID>"')
-		await client.send_message(message.channel, 'Finding NPC :\n- "!findnpc <NAME/ID>" or "!fn <NAME/ID>"')
+		await message.channel.send('Find item\'s tooltip :\n- "!finditem <NAME/ID>" or "!fi <NAME/ID>" - Example -> !finditem thunderfury\n- "!finditem #VANILLAGAMINGITEMID" - Example -> !finditem 18402')
+		await message.channel.send('Finding Player :\n- "!findplayer <NAME>" or "!fp <NAME>"')
+		await message.channel.send('Finding Quest :\n- "!findquest <NAME/ID>" or "!fq <NAME/ID>"')
+		await message.channel.send('Finding NPC :\n- "!findnpc <NAME/ID>" or "!fn <NAME/ID>"')
 	elif (message.content.startswith(('!finditem', '!fi', '!i', '!item'))):
-		await client.send_message(message.channel, 'Looking for item...')
+		await message.channel.send('Looking for item...')
 		foundFile = False
 		delete = True
 		try:
@@ -48,7 +46,7 @@ async def on_message(message):
 						itemid = findItemIDFromName(newArgs[1])
 				else:
 					itemid = findItemIDFromName(rebuildName(newArgs))
-				await client.send_message(message.channel, str(dbUrl + str(itemid)))
+				await message.channel.send(str(dbUrl + str(itemid)))
 				if findimagefromcache(itemid):
 					delete = False
 				else:
@@ -56,31 +54,33 @@ async def on_message(message):
 					takeimage(itemid)
 				try:
 					with open(cachefolder + itemid + '.png', 'rb') as f:
-						await client.send_file(message.channel, f)
+						await message.channel.send(file=discord.File(cachefolder + itemid + '.png'))
 						foundFile = True
 				except:
-					await client.send_message(message.channel, 'Error Finding Item, make sure you pass the right item ID')
+                                        print(traceback.format_exc())
+                                        await message.channel.send('Error Finding Item, make sure you pass the right item ID')
 			else:
-				await client.send_message(message.channel, 'Command Error')
+				await message.channel.send('Command Error')
 		except ValueError:
-			await client.send_message(message.channel, 'Error Finding Item, make sure you passed the right parameters')
+			print(traceback.format_exc())
+			await message.channel.send('Error Finding Item, make sure you passed the right parameters')
 		#If cache argument has not passed, delete the item after sending it to Discord
 		if delete and foundFile and cachetrigger is False:
 			os.remove(cachefolder + str(itemid) + '.png')
 			print(str(itemid) + '.png removed ')
 	elif (message.content.startswith(('!findplayer', '!fp', '!p', '!player'))):
-		await client.send_message(message.channel, 'Looking for Player...')
+		await message.channel.send('Looking for Player...')
 		try:
 			newArgs = message.content.split(' ')
 			if (len(newArgs) == 2):
 				try:
-					await client.send_message(message.channel, findplayer(newArgs[1]))
+					await message.channel.send(findplayer(newArgs[1]))
 				except:
-					await client.send_message(message.channel, "Couldn't find player")
+					await message.channel.send("Couldn't find player")
 		except:
-			await client.send_message(message.channel, 'Command Error')
+			await message.channel.send('Command Error')
 	elif (message.content.startswith(('!findquest', '!fq', '!q', '!quest'))):
-		await client.send_message(message.channel, 'Looking for Quest...')
+		await message.channel.send('Looking for Quest...')
 		try:
 			newArgs = message.content.split(' ')
 			if (len(newArgs) == 2):
@@ -88,17 +88,17 @@ async def on_message(message):
 					itemid = newArgs[1]
 				else:
 					itemid = findQuestIDFromName(newArgs[1])
-			else:		
-				itemid = findQuestIDFromName(rebuildName(newArgs))			
-			if (itemid):
-				await client.send_message(message.channel, str(dbQuestUrl + str(itemid)))
 			else:
-				await client.send_message(message.channel, "Couldn't find quest")
+				itemid = findQuestIDFromName(rebuildName(newArgs))
+			if (itemid):
+				await message.channel.send(str(dbQuestUrl + str(itemid)))
+			else:
+				await message.channel.send("Couldn't find quest")
 		except Exception:
 			print(traceback.format_exc())
-			await client.send_message(message.channel, "Couldn't find quest")
+			await message.channel.send("Couldn't find quest")
 	elif (message.content.startswith(('!findnpc', '!fn', '!n', '!npc'))):
-		await client.send_message(message.channel, 'Looking for NPC...')
+		await message.channel.send('Looking for NPC...')
 		try:
 			newArgs = message.content.split(' ')
 			if (len(newArgs) == 2):
@@ -106,31 +106,27 @@ async def on_message(message):
 					itemid = newArgs[1]
 				else:
 					itemid = findNpcIDFromName(newArgs[1])
-			else:		
-				itemid = findNpcIDFromName(rebuildName(newArgs))			
-			if (itemid):
-				await client.send_message(message.channel, str(dbNpcUrl + str(itemid)))
 			else:
-				await client.send_message(message.channel, "Couldn't find npc")
+				itemid = findNpcIDFromName(rebuildName(newArgs))
+			if (itemid):
+				await message.channel.send(str(dbNpcUrl + str(itemid)))
+			else:
+				await message.channel.send("Couldn't find npc")
 		except Exception:
 			print(traceback.format_exc())
-			await client.send_message(message.channel, "Couldn't find npc")	
+			await message.channel.send("Couldn't find npc")
 
 def takeimage(itemID):
-	#setup browser for running in background
-	os.environ['MOZ_HEADLESS'] = '1'
-	binary = FirefoxBinary(FIREFOX_PATH, log_file=sys.stdout)
-	binary.add_command_line_options('-headless')
-	browser = webdriver.Firefox(executable_path=GECKODRIVER_PATH,firefox_binary=binary)
-	#request item url
-	browser.get(dbUrl + itemID)
-
-	try:
-		browser.find_element_by_class_name('tooltip').screenshot(cachefolder + str(itemID) + '.png')
-		print('Tooltip for item id : %s found at %s\nSaved at %s' % (itemID, str(dbUrl + str(itemID)), str(cachefolder+ str(itemID) + '.png')))
-	except:
-		print('Tooltip for item id : %s not found at %s' % (itemID, str(dbUrl + str(itemID))))
-	browser.close()
+    #setup browser for running in background
+    os.environ['MOZ_HEADLESS'] = '1'
+    browser = webdriver.Firefox()
+    browser.get(dbUrl + itemID)
+    try:
+        browser.find_element_by_class_name('tooltip').screenshot(cachefolder + str(itemID) + '.png')
+        print('Tooltip for item id : %s found at %s\nSaved at %s' % (itemID, str(dbUrl + str(itemID)), str(cachefolder+ str(itemID) + '.png')))
+    except:
+        print('Tooltip for item id : %s not found at %s' % (itemID, str(dbUrl + str(itemID))))
+    browser.close()
 
 
 def findplayer(playerName):
@@ -204,12 +200,12 @@ def rebuildName(args):
 if __name__ == '__main__':
 	myargs = sys.argv
 	if '-nc' in myargs:
-		cachetrigger = False
-		if not os.path.exists(os.path.dirname(cachefolder)):
-		    try:
-		        os.makedirs(os.path.dirname(cachefolder))
-		    except:
-		        print('Error while creating the cache folder')
+            cachetrigger = False
+	if not os.path.exists(os.path.dirname(cachefolder)):
+	    try:
+	        os.makedirs(os.path.dirname(cachefolder))
+	    except:
+	        print('Error while creating the cache folder')
 	print('Cache is {0}'.format(cachetrigger))
 	print(myargs)
 	global items, quests, npcs
